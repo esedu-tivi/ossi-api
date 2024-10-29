61ad85cce83aad5f5b66f9ab37aa201da1a70e0b
Ilari O. <146765325+yolthurix@users.noreply.github.com>
2024-10-29 08:56:15 +0000

    add null coalesce to api-gateway/config.ts

--- api-gateway/src/config.ts
+++ api-gateway/src/config.ts
@@ -1,9 +1,9 @@
 import 'dotenv/config'
 
 const config = { 
-    MS_TENANT_ID: process.env.MS_TENANT_ID,
-    MS_CLIENT_ID: process.env.MS_CLIENT_ID,
-    JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
+    MS_TENANT_ID: process.env.MS_TENANT_ID ?? "",
+    MS_CLIENT_ID: process.env.MS_CLIENT_ID ?? "",
+    JWT_SECRET_KEY: process.env.JWT_SECRET_KEY ?? "",
 }
 
 export { config }
