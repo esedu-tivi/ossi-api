3a11e319ce211b9d1f39342f37fccecdaeb60aee
Ilari O. <146765325+ylthr@users.noreply.github.com>
2025-01-10 13:41:21 +0200

    remove description and tags from project resolver

--- api-gateway/src/graphql/resolvers/project.ts
+++ api-gateway/src/graphql/resolvers/project.ts
@@ -7,20 +7,6 @@ const includedInQualificationUnitParts: Resolver<{ id: number }, null> = async (
     return response.data;
 }
 
-const description: Resolver<{ id: number }, null> = async (parent, _, context) => {
-    const response = await axios.get(process.env.INTERNAL_STUDENT_MANAGEMENT_API_URL + `/qualification/projects/${parent.id}/description`);
-
-    return response.data.description;
-}
-
-const tags: Resolver<{ id: number }, null> = async (parent, _, context) => {
-    const response = await axios.get(process.env.INTERNAL_STUDENT_MANAGEMENT_API_URL + `/qualification/projects/${parent.id}/tags`);
-
-    return response.data;
-}
-
 export const QualificationProject = {
     includedInQualificationUnitParts,
-    description,
-    tags,
 }
