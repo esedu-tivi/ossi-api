b9beee1a811350b9aa57e8b5bb4c00023995a168
Ilari O. <146765325+ylthr@users.noreply.github.com>
2025-02-12 13:09:33 +0200

    amend to previous commit

--- student-management-api/src/index.ts
+++ student-management-api/src/index.ts
@@ -1,4 +1,16 @@
 import 'dotenv/config';
 import app from "./app";
+import { QualificationUnit } from 'sequelize-models/dist/qualification-unit';
+import { QualificationCompetenceRequirement, QualificationCompetenceRequirements } from 'sequelize-models';
+import { getExternalQualificationData } from './utils/utils';
 
-app.listen(3000);
+(async () => {
+    const qualificationData = await getExternalQualificationData(7861752);
+
+    await QualificationUnit.bulkCreate(qualificationData.units);
+    await QualificationCompetenceRequirements.bulkCreate(qualificationData.competenceRequirementGroups);
+    await QualificationCompetenceRequirement.bulkCreate(qualificationData.competenceRequirements);
+
+    app.listen(3000);
+})
+
