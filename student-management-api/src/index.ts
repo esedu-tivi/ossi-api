ac9ed3f2f54c33f4b8933fea75fb26bad6f92f44
Ilari O. <146765325+ylthr@users.noreply.github.com>
2025-02-13 09:36:37 +0200

    fix duplication of qualification data on startup

--- student-management-api/src/index.ts
+++ student-management-api/src/index.ts
@@ -5,11 +5,14 @@ import { QualificationCompetenceRequirement, QualificationCompetenceRequirements
 import { getExternalQualificationData } from './utils/utils';
 
 (async () => {
-    const qualificationData = await getExternalQualificationData(7861752);
+    // we can presume qualification data from ePeruste is not yet, if there are no units in db
+    if ((await QualificationUnit.findAll()).length == 0) {
+        const qualificationData = await getExternalQualificationData(7861752);
 
-    await QualificationUnit.bulkCreate(qualificationData.units);
-    await QualificationCompetenceRequirements.bulkCreate(qualificationData.competenceRequirementGroups);
-    await QualificationCompetenceRequirement.bulkCreate(qualificationData.competenceRequirements);
+        await QualificationUnit.bulkCreate(qualificationData.units);
+        await QualificationCompetenceRequirements.bulkCreate(qualificationData.competenceRequirementGroups);
+        await QualificationCompetenceRequirement.bulkCreate(qualificationData.competenceRequirements);
+    }
 
     app.listen(3000);
 })()
