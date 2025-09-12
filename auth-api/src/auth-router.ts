2455650aec0f6d7662b9fa86af2bce7b40c96841
Jani Kaartinen <jkaart@gmail.com>
2025-09-12 12:07:19 +0300

    Auth-api works now with the Prisma ORM

--- auth-api/src/auth-router.ts
+++ auth-api/src/auth-router.ts
@@ -1,7 +1,12 @@
 import axios from "axios";
 import express, { json } from "express";
 import jwt, { JwtPayload } from "jsonwebtoken";
-import { sequelize, Student, Teacher, User, UserAuthorityScope } from "sequelize-models";
+//import { sequelize, Student, Teacher, User, UserAuthorityScope } from "sequelize-models";
+import { PrismaClient, enumUsersScope } from "prisma-orm"
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
+const prisma = new PrismaClient({ adapter })
 
 interface IdTokenPayload extends JwtPayload {
     oid: string,
@@ -11,11 +16,11 @@ interface IdTokenPayload extends JwtPayload {
     upn: string,
 }
 
-async function getPemCertificate(idToken) { 
+async function getPemCertificate(idToken) {
     const jwks = (await axios.get("https://login.microsoftonline.com/common/discovery/keys")).data;
 
     const idTokenKid = jwt.decode(idToken, { complete: true }).header.kid;
-    
+
     const jwksKey = jwks["keys"].find(key => key.kid == idTokenKid);
 
     return "-----BEGIN CERTIFICATE-----\n" + jwksKey.x5c[0] + "\n-----END CERTIFICATE-----";
@@ -27,68 +32,93 @@ router.use(json());
 
 // intended for basic ossi login, job supervisor scopes should be created in a seperate endpoint?
 router.post("/login", async (req, res) => {
-    const transaction = await sequelize.transaction();
-    await sequelize.query("LOCK TABLE \"users\" IN ACCESS EXCLUSIVE MODE", { transaction });
-    
-    let idToken: IdTokenPayload;
-    try {
-        const pem = await getPemCertificate(req.body.idToken);
-        idToken = jwt.decode(req.body.idToken) as IdTokenPayload; //idToken = jwt.verify(req.body.idToken, pem, { algorithms: ["RS256"] }) as IdTokenPayload;
-    } catch (e) {
-        console.log(e);
-        return res.json({
-            status: 401,
-            success: false,
-            message: "Error while verifying ID token, logged."
-        });
-    }
+    const userData = await prisma.$transaction(async (transaction) => {
 
-    const isUserInDatabase = await User.findOne({ where: { oid: idToken.oid }, transaction }) != null;
-    const userScope = idToken.upn.endsWith("@esedulainen.fi") ? UserAuthorityScope.Student : UserAuthorityScope.Teacher;
-    
-    // create user and teacher or student rows for nonexistant user
-    if (!isUserInDatabase) {
-        const createdUser = await User.create({
-            oid: idToken.oid,
-            isSetUp: false,
-            firstName: idToken.given_name,
-            lastName: idToken.family_name,
-            email: idToken.upn,
-            phoneNumber: "",
-            scope: userScope,
-            archived: false,
-        }, { transaction });
+        // Prisma ORM do not have table lock functionality built-in, so need use raw query
+        await transaction.$queryRaw`LOCK TABLE "users" IN ACCESS EXCLUSIVE MODE`
 
-        if (userScope == UserAuthorityScope.Student) {
-            await Student.create({
-                id: createdUser.id,
-                groupId: idToken.jobTitle,
-                qualificationCompletion: null,
-                qualificationTitleId: null, 
-                qualificationId: null
-            }, { transaction });
-        } else if (userScope == UserAuthorityScope.Teacher) {
-            await Teacher.create({
-                id: createdUser.id,
-                teachingQualificationTitleId: null,
-                teachingQualificationId: null
-            }, { transaction });
+        //    const transaction = await sequelize.transaction();
+        //    await sequelize.query("LOCK TABLE \"users\" IN ACCESS EXCLUSIVE MODE", { transaction });
+
+        let idToken: IdTokenPayload;
+        try {
+            const pem = await getPemCertificate(req.body.idToken);
+            idToken = jwt.decode(req.body.idToken) as IdTokenPayload; //idToken = jwt.verify(req.body.idToken, pem, { algorithms: ["RS256"] }) as IdTokenPayload;
+        } catch (e) {
+            console.log(e);
+            return res.json({
+                status: 401,
+                success: false,
+                message: "Error while verifying ID token, logged."
+            });
         }
+
+        const isUserInDatabase = await transaction.user.findFirst({ where: { oid: idToken.oid } }) != null;
+        const userScope = idToken.upn.endsWith("@esedulainen.fi") ? enumUsersScope.STUDENT : enumUsersScope.TEACHER;
+
+        // create user and teacher or student rows for nonexistant user
+        if (!isUserInDatabase) {
+            const createdUser = await transaction.user.create({
+                data: {
+                    oid: idToken.oid,
+                    isSetUp: false,
+                    firstName: idToken.given_name,
+                    lastName: idToken.family_name,
+                    email: idToken.upn,
+                    phoneNumber: "",
+                    scope: userScope,
+                    archived: false,
+                }
+
+            })
+
+            if (userScope === enumUsersScope.STUDENT) {
+                await transaction.student.create({
+                    data: {
+                        userId: createdUser.id,
+                        groupId: idToken.jobTitle,
+                        qualificationCompletion: null,
+                        qualificationTitleId: null,
+                        qualificationId: null
+                    }
+                });
+            } else if (userScope === enumUsersScope.TEACHER) {
+                await transaction.teacher.create({
+                    data: {
+                        userId: createdUser.id,
+                        teachingQualificationTitleId: null,
+                        teachingQualificationId: null
+                    }
+                });
+            }
+        }
+
+        const user = await transaction.user.findFirst({ where: { oid: idToken.oid } });
+
+        //Need use findUnique because we do not have findByPk() in the Prisma ORM
+        const profile = userScope == enumUsersScope.STUDENT
+            ? await transaction.student.findUnique({ where: { userId: user.id } })
+            : await transaction.teacher.findUnique({ where: { userId: user.id } });
+
+        const userData = {
+            id: user.id,
+            oid: user.oid,
+            email: user.email,
+            isSetUp: user.isSetUp,
+            type: idToken.upn.endsWith("@esedulainen.fi") ? "STUDENT" : "TEACHER",
+            scope: userScope,
+            profile
+        };
+
+        return userData
+    });
+
+    //If Prisma ORM rollback transaction and we do not have userData
+    if (!userData) {
+        return res.json({ status: 200, success: false })
     }
 
-    const user = await User.findOne({ where: { oid: idToken.oid }, transaction });
-    const profile = userScope == UserAuthorityScope.Student ? await Student.findByPk(user.id, { transaction }) : await Teacher.findByPk(user.id, { transaction });
-
-    const userData = {
-        id: user.id,
-        oid: user.oid,
-        email: user.email,
-        isSetUp: user.isSetUp,
-        type: idToken.upn.endsWith("@esedulainen.fi") ? "STUDENT" : "TEACHER",
-        scope: userScope,
-        profile: profile
-    };
-
+    // In Prisma ORM we do not need manually commit transaction, so we can return response
     res.json({
         status: 200,
         success: true,
@@ -96,8 +126,6 @@ router.post("/login", async (req, res) => {
             expiresIn: "1d"
         }),
     });
-
-    await transaction.commit();
-});
+})
 
 export const AuthRouter = router;
