09e6d6a2dbe4265e58b275050dff8eff777cdc95
Jani Kaartinen <jkaart@gmail.com>
2025-11-03 15:42:51 +0200

    removed unnecessary pg dependencies and postgres pools

--- student-management-api/src/postgres-pool.ts
+++
@@ -1,9 +0,0 @@
-import pg from "pg";
-
-export const pool = new pg.Pool({
-    host: process.env.NODE_ENV === 'test' ? "db-test" : "db",
-    port: 5432,
-    user: "postgres",
-    password: "postgres",
-    database: "postgres"
-})
